6b71fed979a8bfcbfc4e81c58192e5a69b11fb52
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-07-18 23:50:02 -0300

    iniciando projeto django

---
+++ greed/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('HomePage')
+  
