4d3aa65c158e611c8f4a66e7e3deb95504f0edcb
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-07-23 22:44:45 -0300

    tratando expect e validacoes

--- greed/urls.py
+++ greed/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('getTime_table', views.getTime_table, name='getTime_table')
+    path('getTime_table', views.getTime_table, name='getTime_table'),
+    path('getFile', views.getFile, name='getFile')
     
 ]
--- greed/views.py
+++ greed/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, redirect
 from itertools import zip_longest
 from greed.intervalscheduling import *
 from datetime import datetime
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 def group_elements(n, iterable, padvalue='x'):
@@ -21,16 +23,16 @@ def querydict_dict(start, end , weight):
     
 
 def getTime_table(request):
-    
+
     postdata = []
-    
+
     if request.method == 'POST':
-        
+
         postdata = request.POST.dict()
         postdata.pop('csrfmiddlewaretoken')
         print(postdata)
         print(len(postdata))
-        
+
         keysList = list(postdata.values()) ## CHAMAR FUNCAO REMOVE QUOTATION
         print(keysList)
 
@@ -38,30 +40,45 @@ def getTime_table(request):
         list_quatation = removeQuotation(keysList)
         print('printando list quotation')
         print(list_quatation)
-        
+
 
         print('printando new list quotation')
         print(list_quatation)    
-          
-            
+
+
         I = []
-        for output in group_elements(4,list_quatation):
-            I.append(output)
+        try:
+            for output in group_elements(4,list_quatation):
+                I.append(output)
 
-        print('printando I')
-        print(I)
+            print('printando I')
+            print(I)
 
-        ## CHAMA FUNCAO RESOLVE
+            ## CHAMA FUNCAO RESOLVE
 
-        list_resolve = []
-        list_resolve = resolve(I)
-        print(list_resolve)
+            list_resolve = []
+            list_resolve = resolve(I)
+            print(list_resolve)
+        except ValueError as erro:
+            messages.info(request, 'Por favor certifique se todos os campos foram preenchidos')
+            return HttpResponseRedirect('/home')
 
-        weightedinterval = WeightedIntervalScheduling(list_resolve)
-        max_weight, best_intervals = weightedinterval.weighted_interval()
-        current_date = datetime.now().date()
 
-        best_intervals = replaceTime(best_intervals)
-          
+        try:
+            weightedinterval = WeightedIntervalScheduling(list_resolve)
+            max_weight, best_intervals = weightedinterval.weighted_interval()
+        except RecursionError as erro:
+            messages.info(request, 'O horário inicial deve ser menor que o horário final')
+            return HttpResponseRedirect('/home')
+            
+        else:       
+            current_date = datetime.now().date()
+
+            best_intervals = replaceTime(best_intervals)
+
     return render(request,'scheduling.html', {'best_intervals': best_intervals, 'current_date': current_date})
+
+def getFile(request):
     
+    if request.method == 'POST':
+        print('entrou')    
